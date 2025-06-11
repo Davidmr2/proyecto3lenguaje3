9926c72d16d5e1871f71b8166432eac0ceeb4a02
Davidmr2 <cuenta2mryt@gmail.com>
2025-06-11 10:05:12 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace proyecto3lorenzo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
