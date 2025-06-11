3c99be432fd080c807790aabf5aa2eac0fb46307
Davidmr2 <cuenta2mryt@gmail.com>
2025-06-11 10:00:41 -0400

    Actualización completa del proyecto con vistas, modelo y controlador

---
+++ Actividad3LengProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,74 @@
+﻿using Actividad3LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad3LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(estudiante);
+                return RedirectToAction("Lista");
+            }
+            return View("Index", estudiante);
+        }
+
+        public ActionResult Lista()
+        {
+            return View(estudiantes);
+        }
+
+        public ActionResult Editar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante == null)
+                return HttpNotFound();
+            return View(estudiante);
+        }
+
+		private ActionResult HttpNotFound()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+        public ActionResult Editar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                var original = estudiantes.FirstOrDefault(e => e.Matricula == estudiante.Matricula);
+                if (original != null)
+                {
+                    original.Nombre = estudiante.Nombre;
+                    original.Edad = estudiante.Edad;
+                    original.Correo = estudiante.Correo;
+                    original.Carrera = estudiante.Carrera;
+                }
+                return RedirectToAction("Lista");
+            }
+            return View(estudiante);
+        }
+
+        public ActionResult Eliminar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante != null)
+                estudiantes.Remove(estudiante);
+
+            return RedirectToAction("Lista");
+
+
+		}
+	}
+
+}
---
+++ Actividad3LengProg3/Models/EstudianteViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengProg3.Models
+{
+	public class EstudianteViewModel
+	{
+		[Required]
+		[StringLength(10)]
+		public string Matricula { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Nombre { get; set; }
+
+		[Required]
+		[Range(1, 100)]
+		public int Edad { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Correo { get; set; }
+
+		[Required]
+		public string Carrera { get; set; }
+	}
+}
+
