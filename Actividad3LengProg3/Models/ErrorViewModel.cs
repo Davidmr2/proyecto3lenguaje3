d403c55eaf30bd2cb86eeaf80f601a60913c217e
Davidmr2 <cuenta2mryt@gmail.com>
2025-06-13 10:00:49 -0400

    Delete Actividad3LengProg3 directory

--- Actividad3LengProg3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Actividad3LengProg3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
